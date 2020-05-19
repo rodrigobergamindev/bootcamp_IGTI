026d4b7889904ccf80baf92281062466c6c0e946
Rodrigo <61331491+rodrigobergamindev@users.noreply.github.com>
2020-05-19 18:29:03 -0300

    commit

--- modulo_01/fundamentos/projeto/js/script.js
+++ modulo_01/fundamentos/projeto/js/script.js
@@ -1,37 +1,9 @@
-/*const r = document.querySelector('#r')
-const g = document.querySelector('#g')
-const b = document.querySelector('#b')
-
-const rInput = document.querySelector('#r2')
-const gInput = document.querySelector('#g2')
-const bInput = document.querySelector('#b2')
-
-const color = (event) => {
-    const hexadecimal = event.target.value
-    console.log(hexadecimal)
-    
-
-}
-
-function calculator() {
-r.addEventListener("change", color);
-g.addEventListener("change", color);
-b.addEventListener("change", color);
-}
-
-calculator()
-*/
-
-const inputRange = document.querySelectorAll('input[type=range]')
-const inputText = document.querySelectorAll('input[type=text]')
-const spectre = document.querySelector('.spectre')
-
-
 function calculator(inp, result, spectre) {
 
 
 result.forEach(item => {
-    item.value = 125
+    item.value = 0
+    item.style.textAlign = 'center'
 })
 
 spectre.style.backgroundColor = `rgb(
@@ -58,9 +30,16 @@ function color(event) {
 
 
 inp.forEach(input => {
+    input.value = 0
     input.addEventListener("change", color)
 })
 
 }
 
+const inputRange = document.querySelectorAll('input[type=range]')
+const inputText = document.querySelectorAll('input[type=text]')
+const spectre = document.querySelector('.spectre')
 calculator(inputRange, inputText, spectre)
+
+
+
