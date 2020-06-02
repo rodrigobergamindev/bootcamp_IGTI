6682c0ca485b4a54f9ac95ad01b4f87d905494c9
Rodrigo <rodrigobergamindev@gmail.com>
2020-06-02 16:15:49 -0300

    commit

--- modulo_02/my-bank-api/index.js
+++ modulo_02/my-bank-api/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
+})
+
+app.post('/account', (req,res) => {
+    const params = req.body;
+    fs.writeFile("accounts.json", JSON.stringify(params), err => console.log(err))
 })
 
 app.listen(port, () => {
