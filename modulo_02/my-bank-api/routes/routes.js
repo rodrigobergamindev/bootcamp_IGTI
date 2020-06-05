901017a4e2b8ff44a1680807950c2bc4bbc78c22
Rodrigo <rodrigobergamindev@gmail.com>
2020-06-05 10:39:39 -0300

    commit

--- modulo_02/my-bank-api/routes/routes.js
+++ modulo_02/my-bank-api/routes/routes.js
@@ -38,8 +38,32 @@ router.route('/accounts')
             }
         })
     })
+    .put((req, res) => {
+        const newAccount = {
+            id : parseInt(req.body.id),
+            name: req.body.name,
+            balance: parseFloat(req.body.balance)
+        }
+    
+        fs.readFile('./accounts.json', 'utf8', (err,data) => {
+            if(!err) {
+                const clients = JSON.parse(data)
+                const accounts = clients.accounts
+                const myAccount = accounts.find(acc => acc.id === newAccount.id)
+                const accIndex = accounts.indexOf(myAccount)
+                
+                accounts.splice(accIndex, 1, newAccount)
+                fs.writeFile('./accounts.json', JSON.stringify(clients), err =>  console.log(err))
+                
+            }
+            console.log(err)
+        })
+        res.send('account updated!')
+    })
 
-router.get('/accounts/:id', (req,res) => {
+
+router.route('/accounts/:id')
+    .get((req,res) => {
     fs.readFile('./accounts.json', 'utf8', (err,data) => {
         if(!err) {
             const id = req.params.id
@@ -50,24 +74,82 @@ router.get('/accounts/:id', (req,res) => {
         console.log(err)
     })
 })
-
-/*
-router.delete('/accounts/:id', (req, res) => {
+    .delete((req, res) => {
 
     fs.readFile('./accounts.json', 'utf8', (err,data) => {
         if(!err) {
-            const id = req.params.id
+            const id = parseInt(req.params.id)
             const clients = JSON.parse(data)
             const accounts = clients.accounts
             const account = accounts.find(acc => acc.id === id)
-            console.log(accounts)
-
+            const accIndex = accounts.indexOf(account)
+            
+            accounts.splice(accIndex, 1)
+            fs.writeFile('./accounts.json', JSON.stringify(clients), err =>  console.log(err))
+            
         }
         console.log(err)
     })
-
+    res.send('account deleted')
 })
-*/
+
+router.put('/draw', (req,res) => {
+
+    const account = {
+        id : parseInt(req.body.id),
+        draw: parseFloat(req.body.draw)
+    }
+
+    fs.readFile('./accounts.json', 'utf8', (err,data) => {
+        if(!err) {
+            const clients = JSON.parse(data)
+            const accounts = clients.accounts
+            const myAccount = accounts.find(acc => acc.id === account.id)
+
+            if(account.draw){
+                doDraw(myAccount)
+            }
+            
+
+            function doDraw(acc){
+                acc.balance = acc.balance - account.draw
+            }
+
+            fs.writeFile('./accounts.json', JSON.stringify(clients), err =>  console.log(err))
+            res.send(`Draw released, new balance: ${myAccount.balance}`)
+        }
+        console.log(err)
+    })
+})
+
+router.put('/deposit', (req, res) => {
+    const account = {
+        id : parseInt(req.body.id),
+        deposit: parseFloat(req.body.deposit)
+    }
+
+    fs.readFile('./accounts.json', 'utf8', (err,data) => {
+        if(!err) {
+            const clients = JSON.parse(data)
+            const accounts = clients.accounts
+            const myAccount = accounts.find(acc => acc.id === account.id)
+
+            if(account.deposit){
+                doDeposit(myAccount)
+            }
+            
+
+            function doDeposit(acc){
+                acc.balance = acc.balance + account.deposit
+            }
+
+            fs.writeFile('./accounts.json', JSON.stringify(clients), err =>  console.log(err))
+            res.send(`Deposit released, new balance: ${myAccount.balance}`)
+        }
+        console.log(err)
+    })
+})
+
 
 
 module.exports = router
