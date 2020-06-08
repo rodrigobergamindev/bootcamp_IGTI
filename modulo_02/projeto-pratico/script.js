84b9e494b703884dc95f5754e5439d0648b77f67
Rodrigo <rodrigobergamindev@gmail.com>
2020-06-08 13:17:48 -0300

    commit

--- modulo_02/projeto-pratico/script.js
+++ modulo_02/projeto-pratico/script.js
@@ -29,8 +29,10 @@ function main(){
     //countCities('SP')
     //compareCitiesPerState()
     //compareCitiesPerStateTwo()
-    //countCitiesUF() - NÃO COMPLETADO
-    //countCitiesPerUF(states) - MODIFICAR
+    //mostCities(states)
+    //lessCities(states)
+    //longerCityNameAll()
+    //lessCityNameAll()
 }
 
 function countCities (UF){
@@ -75,9 +77,6 @@ function compareCitiesPerState(){
             .filter(city => city.length === longerCityName[0].length)
             .sort()[0]
 
-            
-            const a = 'Santo Antônio do Aracanguá'
-            const b = 'Euclides da Cunha Paulista'
             console.log(longer)
         })
         
@@ -156,23 +155,109 @@ function compareCitiesUF(){
     }
 }
 
-function countCitiesPerUF(states){
-    const cities = states.map(state => {
-        const {
-            sigla,
-            cidades
-        } = state
-        return {
-            sigla: sigla,
-            cities: cidades.length
+function mostCities(states){
+    
+    const mostCities = states.sort((a,b) => a.cidades.length - b.cidades.length)
+    .reverse()
+    .forEach(state => console.log(`${state.sigla} - ${state.cidades.length} `))
+}
+
+function lessCities(states){
+    
+    const mostCities = states.sort((a,b) => a.cidades.length - b.cidades.length)
+    .forEach(state => console.log(`${state.sigla} - ${state.cidades.length} `))
+}
+
+function longerCityNameAll(){
+    
+    fs.readdir('./modulo_02/projeto-pratico/states/', (err, files) => { 
+        if (err) {
+          console.log(err); 
         }
-    })
+        let states = []
+          files.forEach(file => {  
+            states.push(file)
+          }) 
+          count(states)
+      }) 
 
-     const citiesSorted = cities.sort((a,b) => {
-        return (a.cities - b.cities)
-    })
+    function count(states){
+        let arrCities = []
+        let citiesNames = []
 
-    console.log(citiesSorted.reverse())
+        states.forEach(state => {
+            let rawData = fs.readFileSync(`./modulo_02/projeto-pratico/states/${state}`)
+            let states = JSON.parse(rawData)
+            const cities = states.cidades.map(city => `${city.Nome} - ${city.Estado}`)
+            arrCities.push(cities)
+        })
+
+        arrCities.forEach(city => {
+            city.forEach(cityName => citiesNames.push(cityName))
+        })
+        
+        const longerCityNameAll = citiesNames.sort((a,b) => {
+            if(a.length > b.length){
+                return -1
+            }else if(a.length === b.length){
+                if(a > b){
+                    return 1
+                }else{
+                    return -1
+                }
+            }else{
+                return 1
+            }
+        })
+        
+        console.log(longerCityNameAll[0])
+    }
+}
+
+function lessCityNameAll(){
+    
+    fs.readdir('./modulo_02/projeto-pratico/states/', (err, files) => { 
+        if (err) {
+          console.log(err); 
+        }
+        let states = []
+          files.forEach(file => {  
+            states.push(file)
+          }) 
+          count(states)
+      }) 
+
+    function count(states){
+        let arrCities = []
+        let citiesNames = []
+
+        states.forEach(state => {
+            let rawData = fs.readFileSync(`./modulo_02/projeto-pratico/states/${state}`)
+            let states = JSON.parse(rawData)
+            const cities = states.cidades.map(city => `${city.Nome} - ${city.Estado}`)
+            arrCities.push(cities)
+        })
+
+        arrCities.forEach(city => {
+            city.forEach(cityName => citiesNames.push(cityName))
+        })
+        
+        const longerCityNameAll = citiesNames.sort((a,b) => {
+            if(a.length > b.length){
+                return 1
+            }else if(a.length === b.length){
+                if(a > b){
+                    return 1
+                }else{
+                    return -1
+                }
+            }else{
+                return -1
+            }
+        })
+        
+        console.log(longerCityNameAll[0])
+    }
 }
 
 main()
