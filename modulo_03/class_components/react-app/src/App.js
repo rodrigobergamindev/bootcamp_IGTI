82ec889f554a05ebc3acf749354f63a2eaf0c870
Rodrigo <61331491+rodrigobergamindev@users.noreply.github.com>
2020-06-14 23:42:27 -0300

    commit

---
+++ modulo_03/class_components/react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './components/counter/Counter';
+
+function App() {
+  return(
+    <div>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App;
---
+++ modulo_03/class_components/react-app/src/components/counter/IncrementButton.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class IncrementButton extends Component {
+    handleButtonClick = () => {
+        this.props.onIncrement('+')
+    }
+    render() {
+        return (
+            <button onClick={this.handleButtonClick}
+               className="waves-effect
+               waves-light btn grey darken-4">+</button>
+        )
+    }
+}
